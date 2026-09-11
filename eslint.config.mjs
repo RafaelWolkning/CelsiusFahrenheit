3cfedb8d5d61688299d2ef3a1177581deaaa7c73
lucas <lucasaita4000@gmail.com>
2026-09-11 00:28:32 -0300

    chore(lint): lint vue files with eslint-plugin-vue

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,7 @@
 // eslint.config.mjs
+import pluginVue from 'eslint-plugin-vue';
+import globals from 'globals';
+
 export default [
   {
     files: ['src/**/*.js', '__tests__/**/*.js', 'scripts/**/*.js'],
@@ -27,6 +30,26 @@ export default [
       'no-console': 'off',
       'no-undef': 'error',
       'no-redeclare': 'off',
+      'no-debugger': 'warn',
+    },
+  },
+  // Frontend Vue (client/)
+  ...pluginVue.configs['flat/recommended'].map((config) => ({
+    ...config,
+    files: ['client/**/*.{js,vue}'],
+  })),
+  {
+    files: ['client/**/*.{js,vue}'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+      },
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-undef': 'error',
       'no-debugger': 'warn',
     },
   },
